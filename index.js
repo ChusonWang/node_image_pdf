8c704132643ed9e047d872b63fb41b7233242b43
chusonwang <chusonwang@yahoo.com.tw>
2024-08-16 04:14:28 +0800

    Add border and background color

--- index.js
+++ index.js
@@ -23,7 +23,17 @@ async function generateImage(type, output) {
             datasets: [
                 {
                     label: 'Acquisitions by year',
-                    data: data.map(row => row.count)
+                    data: data.map(row => row.count),
+                    borderColor: 'rgb(75, 192, 192)',
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(255, 86, 205)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(255, 86, 205)',
+                      ],
                 }
             ]
         }
